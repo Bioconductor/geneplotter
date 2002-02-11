4862dc2a6016d67d785a5d8f9900380f575112e2
Jeff Gentry <jgentry@jimmy.harvard.edu>
2002-02-11 21:20:49 +0000

    Added function cColor()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@443 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cPlot.R
+++ R/cPlot.R
@@ -1,18 +1,43 @@
-# gPlot.R
+# cPlot.R
 # Will read in a tab deliminated file of genome information
 # and proceed to generate a visual representation of the
 # chromosomes.
 
-plotData <- function(chromNum, locs, scale)
+.plotData <- function(chromNum, locs, xPoints, chromLens, color)
 {
+  # Get the scaling factor
+  scale <- cScale(xPoints, chromLens);
+
   nlocs <- length(locs)
 
-  locs <- locs*scale
+  # APply the scaling factor to the x positions
+  locs <- locs*scale[chromNum]
 
+  # Determine the direction of the Y plot (+ or -)
   ypos <- rep(24-chromNum+1, nlocs)
   ytop <- ifelse(locs>0, ypos+0.4, ypos-0.4)
 
-  segments(abs(locs), ypos, abs(locs), ytop, col="grey")
+  # Plot
+  segments(abs(locs), ypos, abs(locs), ytop, col=color)
+}
+
+cColor <- function(genes, color, xPoints, plotChroms) {
+    # Passed a vector of gene names, a color and an instance of a
+    # chromLocation class.  Will recolor the specific genes in the
+    # cPlot created plot to match the specified color
+
+    # Get the chromLocs listing from the chromLocation class
+    locList <- chromLocs(plotChroms)
+
+    # Look up the locations of these genes in each chromosome,
+    # plotting any results.
+    for (i in 1:nChrom(plotChroms)) {
+        locs <- locList[[i]][genes]
+        locs <- as.numeric(locs[!is.na(locs)])
+        if (length(locs) > 0) {
+            .plotData(i, locs, xPoints, chromLengths(plotChroms), color)
+        }
+    }
 }
 
 cPlot <- function(xPoints, plotChroms) {
@@ -20,13 +45,10 @@ cPlot <- function(xPoints, plotChroms) {
     # points to represent on the X axis, will utilize that data
     # to plot a set of genes on their proper chromosome locations.
 
-    # Get the scaling factor
-    scale <- cScale(xPoints, chromLengths(plotChroms));
-
     # Build the initial plot structure
     plot(c(1, xPoints), c(1,24), type="n", xlab="", ylab="Chromosomes",
     axes=FALSE,)
-    labs <- chromNames(plotChroms)
+    labs <- rev(chromNames(plotChroms))
     axis(2, c(1:nChrom(plotChroms)), labs)
 
     for (i in 1:nChrom(plotChroms)) lines(c(1,xPoints-1),c(i,i),col="blue")
@@ -34,8 +56,10 @@ cPlot <- function(xPoints, plotChroms) {
     byChroms <- chromLocs(plotChroms)
 
     for (i in 1:nChrom(plotChroms)) {
-        plotData(i,byChroms[[i]],scale[i]);
+        .plotData(i,byChroms[[i]], xPoints,
+        chromLengths(plotChroms), "lightgrey");
     }
 }
+
 
 
