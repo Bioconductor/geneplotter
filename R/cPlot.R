74c7ed459fa335c9f903637975dd8c6d1cf3f234
Jeff Gentry <jgentry@jimmy.harvard.edu>
2002-02-14 14:49:18 +0000

    Fixed minor bug in cPlot
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@450 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cPlot.R
+++ R/cPlot.R
@@ -45,8 +45,10 @@ cPlot <- function(plotChroms) {
     # points to represent on the X axis, will utilize that data
     # to plot a set of genes on their proper chromosome locations.
 
+    xPoints <- 1000
+
     # Build the initial plot structure
-    plot(c(1, 1000), c(1,24), type="n", xlab="", ylab="Chromosomes",
+    plot(c(1, xPoints), c(1,24), type="n", xlab="", ylab="Chromosomes",
     axes=FALSE,)
     labs <- rev(chromNames(plotChroms))
     axis(2, c(1:nChrom(plotChroms)), labs)
