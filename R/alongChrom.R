5cfa9cc051c7d4490bf069554cde452b2b8e0280
Jeff Gentry <jgentry@jimmy.harvard.edu>
2002-02-15 03:06:41 +0000

    *** empty log message ***
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@467 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/alongChrom.R
+++ R/alongChrom.R
@@ -1,114 +1,94 @@
-.getCum <- function(eSet, chrom, specChrom) {
+.getGenes <- function(eSet, chrom, specChrom) {
+    ## Will acquire the set of genes used in alongChrom
 
+    ## Extract the gene names of the chromosome of interest
+    cLocs <- chromLocs(specChrom)
+    genes <- cLocs[[chrom]]
+
+    ## Extract out of the expr set the genes that belong on this chrom
+    usedGenes <- genes[names(genes) %in% geneNames(eSet)]
+
+    return(usedGenes)
 }
 
+.getExprs <- function(eSet, usedGenes, cumul) {
+    ## Will get the expression data for the given genes out of the
+    ## expr set.  If cumul is set to TRUE, will generate the
+    ## cumulative sum of this data across the genes.
+
+    ## Split out only the genes on the desired chrom from the exprset
+    chromExprs <- eSet@exprs[names(usedGenes),]
+
+    # Order the columns by the ordering (by location) of the genes
+    ord <- order(abs(usedGenes))
+
+   if (cumul == TRUE) {
+       chromExprs <- t(chromExprs[ord,])
+        ## Fill the matrix with the cumulative sum of the expression
+        chromExprs <- apply(chromExprs, 1, cumsum)
+   }
+   else {
+       chromExprs <- chromExprs[ord,]
+   }
+
+   return(chromExprs)
+}
+
+.identifyLines <- function(xPoints, yPoints) {
+    ## Will call identify() on teh alongChrom() plot to detail which
+    ## lines map tow which samples
+
+    identify(rep(xPoints,ncol(yPoints)), yPoints,
+             labels=rep(colnames(yPoints), rep(nrow(yPoints),ncol(yPoints))))
+}
 
 alongChrom <- function(eSet, chrom, specChrom, index=TRUE, cumul=TRUE,
                      lTypes="1", colors="red", ...) {
-    # Extract the gene names of the chromosome of interest
-    cLocs <- chromLocs(specChrom)
-    genes <- cLocs[[chrom]]
 
-    # Extract out of the expr set the genes that belong on this chrom
-    usedGenes <- genes[names(genes) %in% geneNames(eSet)]
-    chromExprs <- eSet@exprs[names(usedGenes),]
+    ## Will plot a set of exprset samples by genes of a chromosome
+    ## according to their expression levels.
 
-    # Order the columns by the ordering (by location) of the genes
-    ord <- order(abs(usedGenes))
+    ## Get the genes to display
+    usedGenes <- .getGenes(eSet, chrom, specChrom)
 
+    ## Get the expression data, cumulative or otherwise
+    chromExprs <- .getExprs(eSet, usedGenes, cumul)
+
+    ## The Y axis label varies according to if we're taking
+    ## cummulative sums or not
     if (cumul == TRUE) {
-        chromExprs <- t(chromExprs[ord,])
-        ## Fill the matrix with the cumulative sum of the expression
-        chromExprs <- apply(chromExprs, 1, cumsum)
         ylab <- "Cumulative expression levels"
     }
     else {
-        chromExprs <- chromExprs[ord,]
         ylab <- "Expression levels"
     }
 
     ## Plot data
     if (index == TRUE) {
         xPoints <- length(names(usedGenes)) - 1
+        xPoints <- 0:xPoints
         xlim <- xPoints
 
         ## Build main label
         main <- paste(ylab,"by genes in chromosome")
         main <- paste(main,chrom)
-
-        matplot(0:xPoints, chromExprs, type="s", lty=lTypes, col=colors,
-                xlab="", ylab=ylab, main=main, xaxt="n", cex.lab=0.9, ...)
-        axis(1, at=c(0:xPoints), labels = names(usedGenes), las=2,
-             cex.axis=0.7,)
     }
     else {
         xlim <- c(0, chromLengths(specChrom)[as.numeric(chrom)])
         xPoints <- as.numeric(abs(usedGenes)) + 1
-
         ## Build main label
         main <- paste(ylab,"in chromosome")
         main <- paste(main,chrom)
         main <- paste(main,"by relative position.")
-
-        matplot(xPoints, chromExprs, type="s", lty=lTypes, col=colors,
-                xlab="",ylab=ylab, xaxt="n", main=main, cex.lab=0.9, ...)
-        axis(1, at=xPoints, labels = names(usedGenes), las=2,
-             cex.axis=0.7,)
     }
+
+    ## Plot the graph
+    matplot(xPoints, chromExprs, type="s", lty=lTypes, col=colors,
+            xlab="",ylab=ylab, xaxt="n", main=main, cex.lab=0.9, ...)
+    axis(1, at=xPoints, labels = names(usedGenes), las=2,
+         cex.axis=0.7,)
+
+    ## Call identify() on the plot
+    .identifyLines(xPoints, chromExprs)
 }
-
-alongChrom2 <- function(eSet, chrom, specChrom, index=TRUE, cumul=TRUE,
-                     lTypes="1", colors="red", ...) {
-    # Extract the gene names of the chromosome of interest
-    cLocs <- chromLocs(specChrom)
-    genes <- cLocs[[chrom]]
-
-    # Extract out of the expr set the genes that belong on this chrom
-    usedGenes <- genes[names(genes) %in% geneNames(eSet)]
-    chromExprs <- eSet@exprs[names(usedGenes),]
-
-    # Order the columns by the ordering (by location) of the genes
-    ord <- order(abs(usedGenes))
-
-    if (cumul == TRUE) {
-        chromExprs <- t(chromExprs[ord,])
-        ## Fill the matrix with the cumulative sum of the expression
-        chromExprs <- apply(chromExprs, 1, cumsum)
-        ylab <- "Cumulative expression levels"
-    }
-    else {
-        chromExprs <- chromExprs[ord,]
-        ylab <- "Expression levels"
-    }
-
-    ## Plot data
-    if (index == TRUE) {
-        xPoints <- length(names(usedGenes)) - 1
-        xlim <- xPoints
-
-        ## Build main label
-        main <- paste(ylab,"by genes in chromosome")
-        main <- paste(main,chrom)
-
-        plot(0:xPoints, chromExprs, type="s", lty=lTypes, col=colors,
-                xlab="", ylab=ylab, main=main, xaxt="n", cex.lab=0.9, ...)
-        axis(1, at=c(0:xPoints), labels = names(usedGenes), las=2,
-             cex.axis=0.7,)
-    }
-    else {
-        xlim <- c(0, chromLengths(specChrom)[as.numeric(chrom)])
-        xPoints <- as.numeric(abs(usedGenes)) + 1
-
-        ## Build main label
-        main <- paste(ylab,"in chromosome")
-        main <- paste(main,chrom)
-        main <- paste(main,"by relative position.")
-
-        matplot(xPoints, chromExprs, type="s", lty=lTypes, col=colors,
-                xlab="",ylab=ylab, xaxt="n", main=main, cex.lab=0.9, ...)
-        axis(1, at=xPoints, labels = names(usedGenes), las=2,
-             cex.axis=0.7,)
-    }
-}
-
 
