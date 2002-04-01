46c48db401258bd7ff312f73b5ed17d5f2ba2b79
Jeff Gentry <jgentry@jimmy.harvard.edu>
2002-04-01 20:45:32 +0000

    Added options "scale" and "ccol" to cPlot, "method" to cScale and updated their man pages.  Also updated the man pages to chromLoc and chromLocation classes to reflect proper aliasing.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@787 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cPlot.R
+++ R/cPlot.R
@@ -1,38 +1,39 @@
-# cPlot.R
-# Will read in a tab deliminated file of genome information
-# and proceed to generate a visual representation of the
-# chromosomes.
+## cPlot.R
+## Will read in a tab deliminated file of genome information
+## and proceed to generate a visual representation of the
+## chromosomes.
 
-.plotData <- function(chromNum, locs, xPoints, chromLens, color)
+.plotData <- function(chromNum, locs, xPoints, chromLens, color,
+                      scale = c("max","relative")[1])
 {
-  # Get the scaling factor
-  scale <- cScale(xPoints, chromLens);
+  ## Get the scaling factor
+    scale <- cScale(xPoints, chromLens, scale)
 
-  nlocs <- length(locs)
+    nlocs <- length(locs)
 
-  # APply the scaling factor to the x positions
+  ## APply the scaling factor to the x positions
   locs <- locs*scale[chromNum]
 
-  # Determine the direction of the Y plot (+ or -)
+  ## Determine the direction of the Y plot (+ or -)
   ypos <- rep(24-chromNum+1, nlocs)
   ytop <- ifelse(locs>0, ypos+0.4, ypos-0.4)
 
-  # Plot
+  ## Plot
   segments(abs(locs), ypos, abs(locs), ytop, col=color)
 }
 
 cColor <- function(genes, color, plotChroms) {
-    # Passed a vector of gene names, a color and an instance of a
-    # chromLocation class.  Will recolor the specific genes in the
-    # cPlot created plot to match the specified color
+    ## Passed a vector of gene names, a color and an instance of a
+    ## chromLocation class.  Will recolor the specific genes in the
+    ## cPlot created plot to match the specified color
 
     xPoints <- 1000
 
-    # Get the chromLocs listing from the chromLocation class
+    ## Get the chromLocs listing from the chromLocation class
     locList <- chromLocs(plotChroms)
 
-    # Look up the locations of these genes in each chromosome,
-    # plotting any results.
+    ## Look up the locations of these genes in each chromosome,
+    ## plotting any results.
     for (i in 1:nChrom(plotChroms)) {
         locs <- locList[[i]][genes]
         locs <- as.numeric(locs[!is.na(locs)])
@@ -42,14 +43,14 @@ cColor <- function(genes, color, plotChroms) {
     }
 }
 
-cPlot <- function(plotChroms) {
-    # Passed an instance of a chromLocation class, and the number of
-    # points to represent on the X axis, will utilize that data
-    # to plot a set of genes on their proper chromosome locations.
+cPlot <- function(plotChroms, scale=c("max","relative")[1], ccol="lightgrey") {
+    ## Passed an instance of a chromLocation class, and the number of
+    ## points to represent on the X axis, will utilize that data
+    ## to plot a set of genes on their proper chromosome locations.
 
     xPoints <- 1000
 
-    # Build the initial plot structure
+    ## Build the initial plot structure
     plot(c(1, xPoints), c(1,24), type="n", xlab="", ylab="Chromosomes",
     axes=FALSE,)
     labs <- rev(chromNames(plotChroms))
@@ -61,7 +62,7 @@ cPlot <- function(plotChroms) {
 
     for (i in 1:nChrom(plotChroms)) {
         .plotData(i,byChroms[[i]], xPoints, chromLengths(plotChroms),
-                  "lightgrey");
+        ccol, scale);
     }
 }
 
--- R/cScale.R
+++ R/cScale.R
@@ -3,16 +3,21 @@
 # Uses the vector of chromosome lengths and returns a vector
 # of scales.
 
-cScale <- function(points, cLengths) {
+cScale <- function(points, cLengths, method=c("max","relative")[1]) {
 # Passed points - the number of points to scale the chromosomes too
 # and cLengths - a vector of chromosome lengths.
 
     nLen <- length(cLengths);
     cScales <- vector("numeric", length=nLen);
 
-    for (i in 1:nLen) {
-        cScales[i] <- points / cLengths[i];
+    if (method == "max") {
+        for (i in 1:nLen) {
+            cScales[i] <- points / cLengths[i];
+        }
     }
-
+    else {
+        scale = points / max(cLengths)
+        cScales <- rep(scale,nLen)
+    }
     return(cScales);
 }
