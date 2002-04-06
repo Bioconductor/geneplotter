d067057663614c89a0aa7ef3468cb8588859a3ee
Robert Gentleman <rgentlem@fhcrc.org>
2002-04-06 22:43:51 +0000

    fixed this and moved some old plotting code out to the doc directory; might be able to get it back in with some work
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@878 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,4 @@
 .First.lib <- function(libname, pkgname, where)
  { 
-  require(methods)
   require(annotate) 
-  if(missing(where)) {
-        where <- match(paste("package:", pkgname, sep=""), search())
-        if(is.na(where)) {
-            warning(paste("Not a package name: ",pkgname))
-            return()
-        }
-        where <- pos.to.env(where)
-    }
-   .initClasses(where)
 }
