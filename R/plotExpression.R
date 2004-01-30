8bfb2e0c40e3b46881237d222c5785aca92aa068
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-01-30 20:45:40 +0000

    plotExpression code
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@7364 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/plotExpression.R
@@ -0,0 +1,55 @@
+plotExpressionGraph <- function(graph, nodeLLmap, exprs, LOCUSIDenvir,
+                                mapFun, log=FALSE, nodeAttrs=list(), ...) {
+    require("Rgraphviz") || stop("Requires Rgraphviz to continue")
+
+    envll <- unlist(multiget(ls(LOCUSIDenvir), LOCUSIDenvir))
+    graphLLs <- unlist(lapply(IMCALocusLink, function(x){x[1]}))
+    graphAffys <- names(envll)[envll %in% graphLLs]
+
+    if (missing(mapFun))
+        mapFun <- defMapFun
+
+    cols <- getPlotExpressionColors(graphAffys, exprs, mapFun, log)
+
+    ## Vector of colors w/ affy's as names - need SYMs
+    colAffys <- names(cols)
+    colLLs <- envll[colAffys]
+    colSyms <- names(graphLLs[graphLLs %in% colLLs])
+    names(cols) <- colSyms
+    nodeAttrs$fillcolor <- cols
+
+    plot(IMCAGraph, nodeAttrs=nodeAttrs, ...)
+}
+
+
+getPlotExpressionColors <- function(graphAffys, exprs, mapFun, log=FALSE) {
+
+    if (missing(mapFun))
+        mapFun <- defMapFun
+
+    affyCols <- mapFun(exprs, log)
+
+    affyCols[names(affyCols) %in% graphAffys]
+}
+
+defMapFun <- function(exprs, log=FALSE) {
+    part1 <- 100
+    part2 <- 500
+
+    if (log) {
+        part1 <- log2(part1)
+        part2 <- log2(part2)
+    }
+
+    cols <- unlist(lapply(exprs, function(x) {
+        if (x <= part1)
+            "blue"
+        else if (x <= part2)
+            "green"
+        else
+            "red"
+    }))
+
+    cols
+}
+
