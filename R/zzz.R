c00e306a809f6ad5e521acf954e69f928bfc9af8
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-05-05 20:29:20 +0000

    moving to new win menu vig system
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@8527 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
 
     if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
         && .Platform$GUI ==  "Rgui"){
-        addPDF2Vig("geneplotter")
+        addVigs2WinMenu("geneplotter")
     }
 
 
