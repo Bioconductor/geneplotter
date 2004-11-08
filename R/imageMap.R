864b38d4d7d20f04538dbe3f71135bb65fee573b
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2004-11-08 23:00:07 +0000

    bugfix - wh
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@10499 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/imageMap.R
+++ R/imageMap.R
@@ -6,35 +6,22 @@ setMethod("imageMap",
   signature=c("matrix", "connection", "list", "character"),
   definition=function(object, con, tags, imgname) {
     
-  checkTags = function(x, tagname) {
-    if(is.null(names(x))) {
-      if(length(x)==length(nn)) {
-        names(x)=nn
-      } else {
-        stop(paste("'tags$", tagname, "' must have names if it is not the ",
-                   "same length as the number of nodes in 'object'.", sep=""))
-      }
-    } else {
-      if(!all(names(x) %in% nn))
-        stop(paste("'names(tags$", tagname, ")' must match the names of ",
-                   "the nodes in 'object'", sep=""))
-    }
-    return(x)
-  }
-  for(i in seq(along=tags))
-    tags[[i]] = checkTags(tags[[i]], names(tags)[i])
-
-  if(ncol(object)!=4)
+  if(!is.matrix(object)||ncol(object)!=4)
     stop("'object' must be a matrix with 4 columns.")
+
+  for(i in seq(along=tags))
+    if(length(tags[[i]])!=nrow(object))
+      stop(paste("'tags[[", i, "]] must have as many elements as 'object' has rows (",
+                 nrow(object),").", sep=""))
 
   mapname <- paste("map", gsub(" |/|#", "_", imgname), sep="_")
   base::writeLines(paste("<IMG SRC=\"", imgname, "\" USEMAP=\#", mapname, " BORDER=0>", 
                    "<MAP NAME=\"", mapname, "\">", sep=""), con)
   for(i in 1:nrow(object)) {
-    out = paste("<AREA SHAPE=\"rect\" COORDS=\"", coords[i,1], ",", coord[i,2], ",",
-                coords[i,3], ",", coords[i,4], "\"", sep="")
+    out = paste("<AREA SHAPE=\"rect\" COORDS=\"", paste(object[i,], collapse=","),
+                "\"", sep="")
     for(t in seq(along=tags))
-      out = paste(out, " ", names(tags)[i], "=\"", tags[[i]][t], "\"", sep="")
+      out = paste(out, " ", names(tags)[t], "=\"", tags[[t]][i], "\"", sep="")
     out = paste(out, "\">", sep="")
     base::writeLines(out, con)
   }
