a3828aa7a1d40c43cb6eab72b13a26264a6d3e16
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2005-06-09 22:22:02 +0000

    updates - nicer plot
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@12902 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/histStack.R
+++ R/histStack.R
@@ -1,24 +1,19 @@
-histStack <- function(x, breaks, midsFun=paste, ...) {
+histStack <- function(x, breaks, breakFun=paste, ylab="frequency", ...) {
   if(!is.list(x))
     stop("'x' must be a list.")
-  bars <- mids <- NULL
+  bars <- NULL
   for (i in 1:length(x)) {
     if(!is.numeric(x[[i]]))
       paste("Element", i, "of 'x' is not numeric.")
     h <- hist(x[[i]], breaks=breaks, plot=FALSE)
     bars <- rbind(bars, h$counts)
-    if(is.null(mids))
-      mids <- h$mids
-    else
-      stopifnot(identical(mids, h$mids))
   }
 
-  mids <- midsFun(mids)
-  mb <- max(colSums(bars))
-  ylim <- c(0,8)
-  if(mb>8)
-    ylim <- c(0,12)
-  if(mb>12)
-    ylim <- c(0,mb)
-  barplot(bars, names.arg=mids, ylim=ylim, ...)  
+  barplot(bars, names.arg=NULL, space=0, ylab=ylab, ...)
+
+  at     = seq(along=h$breaks)
+  modulo = floor(length(at)/10)
+  sel    = ((at-1) %% modulo == 0)
+  axis(side=1,at=at[sel],labels=breakFun(h$breaks)[sel])
+  browser()
 }
