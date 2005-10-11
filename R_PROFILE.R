3930f61ac1aac3e76e21044514547dbf8df2dafe
Seth Falcon <sfalcon@fhcrc.org>
2005-10-11 00:58:59 +0000

    Remove old style package require stuff.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@14430 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R_PROFILE.R
+++
@@ -1,3 +0,0 @@
-require("methods") || stop("geneplotter requires methods")
-require("Biobase") || stop("geneplotter requires Biobase")
-require("annotate") || stop("geneplotter requires annotate")
--- install.R
+++
@@ -1,2 +0,0 @@
-library("methods")
-library("Biobase")
