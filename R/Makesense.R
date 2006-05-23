da2f8ad38f9e0d0b3fc7174f06ead99d87939965
Seth Falcon <sfalcon@fhcrc.org>
2006-05-23 05:06:39 +0000

    Fixed bug in Makesense so it respects subsetted exprSets
    The code was expecting exprSets containing _every_ probe on the array
    and failed for exprSets that have been subsetted, for example, gone
    through on-specific filtering.  This patch corrects that along with a
    few other minor fixes, for example, we no longer mandate human
    chromosomes and trust the datapkgCHR environment to contain reasonable
    things.
    
    Default color for plotChr changed blue => black.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@18183 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Makesense.R
+++ R/Makesense.R
@@ -3,60 +3,70 @@ setGeneric("Makesense", function(expr, lib, ...) standardGeneric("Makesense"))
 
 setMethod("Makesense", signature(expr="exprSet", lib="character"),
           function(expr, lib, f=1/10) {
-    if (!identical(length(lib), as.integer(1)) || !identical(nchar(lib) > 0, TRUE))
-      stop("No annotation library supplied")
-    Makesense(exprs(expr), lib, f)
+              Makesense(exprs(expr), lib, f)
 })
 
 
 setMethod("Makesense", signature(expr="exprSet", lib="missing"),
           function(expr, f=1/10) {
-              lib <- annotation(expr)
-              Makesense(expr, lib, f)
+              Makesense(expr, annotation(expr), f)
           })
 
 
 setMethod("Makesense", signature(expr="matrix", lib="character"),
           function(expr, lib, f=1/10) {
+    if (length(lib) != 1 || nchar(lib) < 1)
+      stop("invalid arg ", sQuote(lib), " must be lenth one.")
     if(!require(lib, character.only=TRUE))
-        stop("data library ",lib," not avaiable")
+        stop("annotation data package ",lib," not avaiable")
 
+    genes <- rownames(expr)
     libCHR <- paste(lib, "CHR", sep="")
     libCHRLOC <- paste(lib, "CHRLOC", sep="")
-    ##find the positions
-    chr <- contents(get(libCHR))
-    ##for which do we have one and only chromosome no.
-    chrSet <- c(1:22, "X", "Y")
-    oneC <- sapply(chr, function(x) if(x %in% chrSet &&
-                                       length(x)==1) TRUE else FALSE)
-    ##have loc
-    chrL <- contents(get(libCHRLOC))
-    oneL <- sapply(chrL, function(x) if( !is.na(x) &&
-                                        length(x)==1) TRUE else FALSE)
-    chrName <- unlist(chr[oneC&oneL])
-    chrPos <- unlist(chrL[oneC&oneL])
+    ## Select genes that are annotated at exactly _one_ chromosome.
+    chr <- mget(genes, envir=get(libCHR), ifnotfound=NA)
+    oneC <- sapply(chr, function(x)
+                   if (length(x) == 1 && !is.na(x)) TRUE else FALSE)
+    ## Select genes that are annotated at exactly _one_ chrom location
+    ##
+    ## FIXME: There are many genes with multiple CHRLOC entries, is
+    ##        there anything we can do to keep more of them?
+    chrL <- mget(genes, envir=get(libCHRLOC), ifnotfound=NA)
+    oneL <- sapply(chrL, function(x)
+                   if (length(x) == 1 && !is.na(x)) TRUE else FALSE)
+    wanted <- oneC & oneL
+    chrName <- unlist(chr[wanted])
+    chrPos <- unlist(chrL[wanted])
 
     cP <- split(chrPos, chrName)
 
-    gE <- expr[names(oneC&oneL)[oneC&oneL],]
+    gE <- expr[wanted, ]
     ans2 <- vector("list", length=ncol(gE))
     libCHRLENGTHS <- get(paste(lib,"CHRLENGTHS",sep=""))
 
     for( j in 1:ncol(gE) ) {
         s1 <- split(gE[,j], chrName)
-        ans<-NULL
-        for(i in names(cP) ) {
+        ans <- NULL
+        for (i in names(cP)) {
             d1 <- s1[[i]]
             cL <- cP[[i]]
             dp <- d1[cL>0]
             lp <- cL[cL>0]
             dn <- d1[cL<0]
             ln <- -cL[cL<0]
-            lw1 <- lowess(lp, dp, f=f)
-            names(lw1$x) <- names(dp)[order(lp)]
-            lw2 <- lowess(ln, dn, f=f)
-            names(lw2$x) <- names(dn)[order(ln)]
-            lw2$y <- -lw2$y
+            if (length(lp)) {
+                lw1 <- lowess(lp, dp, f=f)
+                names(lw1$x) <- names(dp)[order(lp)]
+            } else {
+                lw2 <- list(x=numeric(0), y=numeric(0))
+            }
+            if (length(ln)) {
+                lw2 <- lowess(ln, dn, f=f)
+                names(lw2$x) <- names(dn)[order(ln)]
+                lw2$y <- -lw2$y
+            } else {
+                lw2 <- list(x=numeric(0), y=numeric(0))
+            }
             ans[[i]] <- list(posS = lw1, negS =lw2)
         }
         ans2[[j]] <- ans
--- R/plotChr.R
+++ R/plotChr.R
@@ -1,9 +1,9 @@
 plotChr <- function(chrN, senseObj,
-	cols=rep("blue", length(senseObj[[1]])),log=FALSE,
+	cols=rep("black", length(senseObj[[1]])),log=FALSE,
 	xloc = c("equispaced", "physical"), geneSymbols=FALSE,
 	ngenes=20, lines.at=NULL, lines.col="red") {
     # lines of +/- stands of a chromosome for a given sample:
-    linesStrand <- function(smooths, col="blue", log=FALSE,
+    linesStrand <- function(smooths, col="black", log=FALSE,
         smX=NULL) {
     	if (is.null(smX)) {
         	sm.px <- smooths$pos$x
@@ -23,15 +23,27 @@ plotChr <- function(chrN, senseObj,
     }
     xloc <- match.arg(xloc)
     ans2 <- senseObj$ans2
+    ## Only plot if we have data
+    numPos <- length(ans2[[1]][[chrN]]$posS$y)
+    numNeg <- length(ans2[[1]][[chrN]]$negS$y)
+    if (numPos < 2)
+      stop("Less than two data points for positive strand on chromosome ",
+           chrN)
+    if (numNeg < 2)
+      stop("Less than two data points for negative strand on chromosome ",
+           chrN)
+    if (numPos < 20 || numNeg < 20)
+      warning("Less than 20 genes annotated on chromosome ", chrN,
+              ".\nConsider using a heatmap instead.")
     libCHRLENGTHS <- get(paste(senseObj$lib,"CHRLENGTHS",sep=""))
     chrRange <- function(chrN) {
-      mn <- min(sapply(ans2, function(x) min(x[[chrN]]$neg$y)))
-      mx <- max(sapply(ans2, function(x) max(x[[chrN]]$pos$y)))
+      mn <- min(sapply(ans2, function(x) min(x[[chrN]]$negS$y)))
+      mx <- max(sapply(ans2, function(x) max(x[[chrN]]$posS$y)))
       c(mn, mx)
     }
     xlims <- c(0, libCHRLENGTHS[chrN])
     ylims <- chrRange(chrN)
-    if( log ) {
+    if( log ) { 
         ylims[1] <- -log(-ylims[1])
         ylims[2] <- log(ylims[2])
     }
@@ -40,7 +52,7 @@ plotChr <- function(chrN, senseObj,
     X <- c(at.px, at.nx)
     uX <- !duplicated(X)
     every <- sum(uX) %/% ngenes
-    if (every < 1) every <- 1
+    if (!isTRUE(every >= 1)) every <- 1
     ind.seq <- seq(1,sum(uX),by=every)
     repGx <- sort(X[uX])[ind.seq]
     probes <- names(X[uX])[order(X[uX])][ind.seq]
@@ -51,8 +63,10 @@ plotChr <- function(chrN, senseObj,
     }
     # probe density:
     if (xloc=="equispaced") {
-	at.px <- approx(sort(X[uX])[ind.seq],repGx, xout=at.px)$y
-        at.nx <- approx(sort(X[uX])[ind.seq],repGx, xout=at.nx)$y
+        if (length(at.px) > 1)
+          at.px <- approx(sort(X[uX])[ind.seq],repGx, xout=at.px)$y
+        if (length(at.nx) > 1)
+          at.nx <- approx(sort(X[uX])[ind.seq],repGx, xout=at.nx)$y
 	smX <- c(at.px, at.nx)
     } else smX <- NULL
     opar <- par(mar = c(6, 5, 4, 1), mgp = c(4, 1, 0))
@@ -68,8 +82,14 @@ plotChr <- function(chrN, senseObj,
     yticks <- pretty(c(0,ylims), 5)
     axis(2, at=yticks, labels=abs(yticks))
     abline(h=0, col="gray")
-    axis(1, at=at.nx, pos=0, tck=-0.01,col="gray", labels=FALSE)
-    axis(1, at=at.px, pos=0, tck=0.01,col="gray", las=3, labels=FALSE)
+    if (length(at.nx))
+      axis(1, at=at.nx, pos=0, tck=-0.01,col="gray", labels=FALSE)
+    else
+      warning("No values on negative strand for chromosome ", chrN)
+    if (length(at.px))
+      axis(1, at=at.px, pos=0, tck=0.01,col="gray", las=3, labels=FALSE)
+    else
+      warning("No values on positive strand for chromosome ", chrN)
     # label representative genes:
     labs <- probes
     if(geneSymbols) labs <- unlist(mget(labs,
