27547ff1a94d815d353c484fa0bb48ebf51bff89
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2007-11-02 05:51:12 +0000

    making sure that openHtmlPage creates a valid HTML file
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@28397 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/openHtmlPage.R
+++ R/openHtmlPage.R
@@ -1,11 +1,14 @@
 openHtmlPage = function(name, title="") {
-  con = file(paste(name, ".html", sep=""), open="wt")
-  writeLines(paste("<html><head><title>", title, "</title></head><body style=\"font-family: helvetica,arial,sans-serif;\">", sep=""), con)
-  return(con)
+    name = gsub("\\.html$", "", name)
+    con = file(paste(name, ".html", sep=""), open="wt")
+    writeLines(paste("<html><head><title>", title,
+                     "</title></head><body style=\"font-family: ",
+                     "helvetica,arial,sans-serif;\">", sep=""), con)
+    return(con)
 }
 
 closeHtmlPage = function(con) {
-  writeLines("</body></html>", con)
-  close(con)
+    writeLines("</body></html>", con)
+    close(con)
 }
 
