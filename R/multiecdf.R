03a63e524a1041cbc31fe0b2a4678eae53d6c152
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2007-11-23 17:57:04 +0000

    I have tried to rationalize the behaviour of "xlab" and "main" argumens of the multiecdf and multidensity functions.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geneplotter@28900 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/multiecdf.R
+++ R/multiecdf.R
@@ -1,87 +1,110 @@
 multiecdf = function(x, ...)
   UseMethod("multiecdf")
+multidensity = function(x, ...)
+  UseMethod("multidensity")
 
-multiecdf.formula <- function(formula, data = NULL, ..., na.action = NULL)
-{
-    if(missing(formula) || (length(formula) != 3))
-        stop("'formula' missing or incorrect")
-    m <- match.call(expand.dots = FALSE)
-    if(is.matrix(eval(m$data, parent.frame())))
-        m$data <- as.data.frame(data)
-    m$... <- NULL
-    m$na.action <- na.action # force use of default for this method
-    m[[1]] <- as.name("model.frame")
-    mf <- eval(m, parent.frame())
-    response <- attr(attr(mf, "terms"), "response")
-    multiecdf(split(mf[[response]], mf[-response]), ...)
+
+multiecdf.formula = function(formula, data = NULL,
+  xlab,
+  na.action = NULL,
+  ...) {
+
+  if(missing(xlab))
+     xlab = deparse(substitute(formula))
+  if(missing(formula) || (length(formula) != 3))
+    stop("'formula' missing or incorrect")
+  if(missing(xlab))
+     xlab = deparse(substitute(formula))
+  m = match.call(expand.dots = FALSE)
+  if(is.matrix(eval(m$data, parent.frame())))
+    m$data = as.data.frame(data)
+  m$... = NULL
+  m$na.action = na.action # force use of default for this method
+  m[[1]] = as.name("model.frame")
+  mf = eval(m, parent.frame())
+  response = attr(attr(mf, "terms"), "response")
+  multiecdf(split(mf[[response]], mf[-response]), xlab=xlab, ...)
 }
 
-multiecdf.matrix <- function(x, ...)
-  multiecdf(x~col(x), ...)
-            
-multiecdf.default <- function(x, xlim, col, do.points=FALSE,
-                              subsample=TRUE, ...) {
+multidensity.formula = function(formula, data = NULL,
+  xlab,
+  na.action = NULL,
+  ...){
+  
+  if(missing(xlab))
+     xlab = deparse(substitute(formula))
+  if(missing(formula) || (length(formula) != 3))
+    stop("'formula' missing or incorrect")
+  m = match.call(expand.dots = FALSE)
+  if(is.matrix(eval(m$data, parent.frame())))
+    m$data = as.data.frame(data)
+  m$... = m$main = m$xlab = NULL
+  m$na.action = na.action # force use of default for this method
+  m[[1]] = as.name("model.frame")
+  mf = eval(m, parent.frame())
+  response = attr(attr(mf, "terms"), "response")
+  multidensity(split(mf[[response]], mf[-response]), xlab=xlab, ...)
+}
+
+
+multiecdf.matrix = function(x, xlab, ...) {
+  if(missing(xlab))
+     xlab = deparse(substitute(x))
+  multiecdf(x~col(x), xlab=xlab, ...)
+}
+
+multidensity.matrix = function(x, xlab, ...) {
+  if(missing(xlab))
+     xlab = deparse(substitute(x))
+  multidensity(x~col(x), xlab=xlab, ...)
+}
+
+
+multiecdf.default = function(x,
+  xlim,
+  col = brewer.pal(9, "Set1"),
+  main = "multiecdf",
+  xlab,
+  do.points = FALSE,
+  subsample = TRUE, ...) {
+  
+  if(missing(xlab))
+     xlab = deparse(substitute(x))
+
   stopifnot(length(x)>=1)
   if(subsample)
     for(i in seq(along=x))
       if(length(x[[i]])>1000)
-        x[[i]] <- x[[i]][sample(1:length(x[[i]]), 1000)]
+        x[[i]] = x[[i]][sample(1:length(x[[i]]), 1000)]
   
   ef = lapply(x, ecdf)
   if(missing(xlim))
     xlim = range(unlist(x), na.rm=TRUE)
-  if(missing(col))
-    col = brewer.pal(9, "Set1")
-  plot(ef[[1]], xlim=xlim, col.hor=col[1], col.vert=col[1], do.points=do.points, ...)
-  m <- match.call(expand.dots = FALSE) # avoid warnings for invalid arguments
-  m$... <- m$...[!names(m$...) %in% c("main", "xlab", "ylab", "ylim")]  
+  plot(ef[[1]], xlim=xlim, xlab=xlab, main=main, col.hor=col[1], col.vert=col[1], do.points=do.points, ...)
+  m = match.call(expand.dots = FALSE) # avoid warnings for invalid arguments
+  m$... = m$...[!names(m$...) %in% c("main", "xlab", "ylab", "ylim")]  
 
   for(j in seq(along=ef)[-1]) {
     mycol = col[1+((j-1)%%length(col))]
-    args <- c(list(x=ef[[j]], col.hor=mycol, col.vert=mycol, do.points=do.points), m$...)
+    args = c(list(x=ef[[j]], col.hor=mycol, col.vert=mycol, do.points=do.points), m$...)
     do.call("lines", args)
 
   }
 }
 
-multidensity = function(x, ...)
-  UseMethod("multidensity")
-
-multidensity.formula = function(formula, data = NULL, main, xlab, ..., na.action = NULL)
-{
-  if(missing(formula) || (length(formula) != 3))
-    stop("'formula' missing or incorrect")
-
-  if(missing(main))
-    main = sprintf("multidensity(%s)", deparse(substitute(formula)))
-  if(missing(xlab))
-    xlab = deparse(substitute(formula))
-  
-  m <- match.call(expand.dots = FALSE)
-  if(is.matrix(eval(m$data, parent.frame())))
-    m$data <- as.data.frame(data)
-  m$... <- m$main <- m$xlab <- NULL
-  m$na.action <- na.action # force use of default for this method
-  m[[1]] <- as.name("model.frame")
-  mf <- eval(m, parent.frame())
-  response <- attr(attr(mf, "terms"), "response")
-  multidensity(split(mf[[response]], mf[-response]), main=main, xlab=xlab, ...)
-}
-
-multidensity.matrix = function(x, xlab, ...) {
-  if(missing(xlab))
-    xlab = deparse(substitute(x))
-  multidensity(x~col(x), xlab=xlab, ...)
-}
-
-multidensity.default = function(x, bw="nrd0", xlim, ylim,
+multidensity.default = function(x,
+  bw="nrd0",
+  xlim,
+  ylim,
   col  = brewer.pal(9, "Set1"),
-  main = sprintf("multidensity.default(%s)", deparse(substitute(x))),
-  xlab = deparse(substitute(x)),
+  main = "multidensity",
+  xlab,
   lty  = 1L , ...) {
+
+  if(missing(xlab))
+     xlab = deparse(substitute(x))
   
   stopifnot(length(x)>=1)
-
   if(missing(xlim))
     xlim = range(unlist(x), na.rm=TRUE)
 
@@ -97,10 +120,10 @@ multidensity.default = function(x, bw="nrd0", xlim, ylim,
     ylim = range(unlist(lapply(ef, "[[", "y")), na.rm=TRUE)
   
   plot(ef[[1]], xlim=xlim, ylim=ylim, xlab=xlab, main=main, col=col[1], lty=lty[1], ...)
-  m <- match.call(expand.dots = FALSE) ## avoid warnings for invalid arguments
-  m$... <- m$...[!names(m$...) %in% c("main", "xlab", "ylab", "ylim")]  
+  m = match.call(expand.dots = FALSE) ## avoid warnings for invalid arguments
+  m$... = m$...[!names(m$...) %in% c("main", "xlab", "ylab", "ylim")]  
   for(j in seq(along=ef)[-1]) {
-    args <- c(list(x=ef[[j]]), col=col[1+((j-1)%%length(col))],
+    args = c(list(x=ef[[j]]), col=col[1+((j-1)%%length(col))],
                                lty=lty[1+((j-1)%%length(lty))], m$...)
     do.call("lines", args)
   }
